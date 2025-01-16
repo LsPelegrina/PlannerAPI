90a551cac83188443fab47e1e65bfc65b875559c
LsPelegrina <lspelegrina@gmail.com>
2025-01-16 02:51:27 -0300

    Feat: Confirm and Invite participants & Update trips

--- src/main/java/com/planner/planner/participant/ParticipantService.java
+++ src/main/java/com/planner/planner/participant/ParticipantService.java
@@ -1,5 +1,7 @@
 package com.planner.planner.participant;
 
+import com.planner.planner.trip.Trip;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -7,10 +9,33 @@ import java.util.UUID;
 
 @Service
 public class ParticipantService {
-    public void registerParticipantsToEvent(List<String> participantsToInvite, UUID tripId) {
 
-    };
+    @Autowired
+    private ParticipantRepository repository;
+
+
+    public void registerParticipantsToEvent(List<String> participantsToInvite, Trip trip) {
+        List<Participant> participants = participantsToInvite.stream().map(email -> new Participant(email, trip)).toList();
+        this.repository.saveAll(participants);
+    }
+
+    @SuppressWarnings("unused")
     public void triggerConfirmationEmailToParticipants(UUID tripId) {
 
-    };
+    }
+
+    @SuppressWarnings("unused")
+    public void triggerConfirmationEmailToParticipant(String email) {
+
+    }
+
+    public ParticipantCreateResponse registerParticipantToEvent(String email, Trip trip){
+        Participant newParticipant = new Participant(email, trip);
+        this.repository.save(newParticipant);
+        return new ParticipantCreateResponse(newParticipant.getId());
+    }
+
+    public List<ParticipantData> getAllParticipantsFromTrip(UUID tripId) {
+        return this.repository.findByTripId(tripId).stream().map(participant -> new ParticipantData(participant.getId(), participant.getName(), participant.getEmail(), participant.isConfirmed())).toList();
+    }
 }
